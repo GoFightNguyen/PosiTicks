c1c5df97970a0872f6c03f462fdd832b46feebba
Jason Nguyen <jason-nguyen@pluralsight.com>
2020-09-20 16:11:12 -0600

    Setup OpenTelemetry and NewRelic as the exporter

--- PosiTicks/Server/Controllers/WeatherForecastController.cs
+++ PosiTicks/Server/Controllers/WeatherForecastController.cs
@@ -27,6 +27,8 @@ namespace PosiTicks.Server.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            System.Diagnostics.Activity.Current.AddTag("testing tag add", "it worked");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
