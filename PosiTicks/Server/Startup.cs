0ad44bbdb47d29ae6122932fbf2285ab95a4e52c
Jason Nguyen <jason-nguyen@pluralsight.com>
2020-09-24 20:30:05 -0600

    Remove NewRelic distributed tracing and open telemetry since it wasn't working properly

--- PosiTicks/Server/Startup.cs
+++ PosiTicks/Server/Startup.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
-using OpenTelemetry.Trace;
-using Microsoft.Extensions.Logging;
 using Serilog;
 
 namespace PosiTicks.Server
@@ -25,23 +23,8 @@ namespace PosiTicks.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
-
-            // https://github.com/newrelic/newrelic-telemetry-sdk-dotnet/blob/master/src/OpenTelemetry.Exporter.NewRelic/README.md
-            // https://github.com/newrelic/newrelic-telemetry-sdk-dotnet/tree/master/examples/OpenTelemetry.Exporter.NewRelic/AspNetCore
-            services.AddOpenTelemetryTracing((serviceProvider, tracerBuilder) =>
-            {
-                // Make the logger factory available to the dependency injection
-                // container so that it may be injected into the OpenTelemetry Tracer.
-                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-
-                tracerBuilder
-                .UseNewRelic(Configuration, loggerFactory)
-                .AddAspNetCoreInstrumentation()
-                .AddHttpClientInstrumentation();
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
