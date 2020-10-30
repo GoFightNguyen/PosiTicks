100103deb865c6d0e3a6afc6a92d4b44d4410214
Jason Nguyen <gofightnguyen.dev@outlook.com>
2020-10-29 19:17:41 -0600

    display validation errors and handle unexpected errors when creating a Class Period

--- PosiTicks/Client/Program.cs
+++ PosiTicks/Client/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
